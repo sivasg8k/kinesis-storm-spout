cfe3fc3c9329eb8916b5e4f927c7414779f23bc8
Sivashankar <sivashankar.g@8kmiles.com>
2015-01-11 18:12:10 +0530

    hbc

--- src/main/java/com/amazonaws/services/kinesis/stormspout/twitterstream/TwitterStreamProducer.java
+++ src/main/java/com/amazonaws/services/kinesis/stormspout/twitterstream/TwitterStreamProducer.java
@@ -4,6 +4,7 @@ import java.nio.ByteBuffer;
 import java.util.List;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.TimeUnit;
 
 import com.amazonaws.services.kinesis.AmazonKinesis;
 import com.amazonaws.services.kinesis.AmazonKinesisClient;
@@ -17,7 +18,9 @@ import com.twitter.hbc.core.Constants;
 import com.twitter.hbc.core.Hosts;
 import com.twitter.hbc.core.HttpHosts;
 import com.twitter.hbc.core.endpoint.StatusesFilterEndpoint;
+import com.twitter.hbc.core.endpoint.StatusesSampleEndpoint;
 import com.twitter.hbc.core.processor.StringDelimitedProcessor;
+import com.twitter.hbc.httpclient.BasicClient;
 import com.twitter.hbc.httpclient.auth.Authentication;
 import com.twitter.hbc.httpclient.auth.OAuth1;
 
@@ -28,12 +31,68 @@ public class TwitterStreamProducer {
 
 	/** Set up your blocking queues: Be sure to size these properly based on expected TPS of your stream */
 	static BlockingQueue<String> msgQueue = new LinkedBlockingQueue<String>(100000);
+	
+	public static void run(String consumerKey, String consumerSecret, String token, String secret) throws InterruptedException {
+	    // Create an appropriately sized blocking queue
+	    BlockingQueue<String> queue = new LinkedBlockingQueue<String>(10000);
 
-	public static void main(String[] args) {
+	    // Define our endpoint: By default, delimited=length is set (we need this for our processor)
+	    // and stall warnings are on.
+	    StatusesSampleEndpoint endpoint = new StatusesSampleEndpoint();
+	    endpoint.stallWarnings(false);
+
+	    Authentication auth = new OAuth1(consumerKey, consumerSecret, token, secret);
+	    //Authentication auth = new com.twitter.hbc.httpclient.auth.BasicAuth(username, password);
+
+	    // Create a new BasicClient. By default gzip is enabled.
+	    BasicClient client = new ClientBuilder()
+	            .name("sampleExampleClient")
+	            .hosts(Constants.STREAM_HOST)
+	            .endpoint(endpoint)
+	            .authentication(auth)
+	            .processor(new StringDelimitedProcessor(queue))
+	            .build();
+
+	    // Establish a connection
+	    client.connect();
+
+	    // Do whatever needs to be done with messages
+	    for (int msgRead = 0; msgRead < 1000; msgRead++) {
+	      if (client.isDone()) {
+	        System.out.println("Client connection closed unexpectedly: " + client.getExitEvent().getMessage());
+	        break;
+	      }
+
+	      String msg = queue.poll(5, TimeUnit.SECONDS);
+	      if (msg == null) {
+	        System.out.println("Did not receive a message in 5 seconds");
+	      } else {
+	        System.out.println(msg);
+	      }
+	    }
+
+	    client.stop();
+
+	    // Print some stats
+	    System.out.printf("The client read %d messages!\n", client.getStatsTracker().getNumMessages());
+	  }
+	
+	public static void main(String args[]) throws InterruptedException {
+		
+		String consumerKey = System.getenv("CONSUMER_KEY");
+		String consumerSecret = System.getenv("CONSUMER_SECRET");
+		String accessToken = System.getenv("ACCESS_TOKEN");
+		String accessTokenSecret = System.getenv("ACCESS_TOKEN_SECRET");
+		
+		TwitterStreamProducer.run(consumerKey, consumerSecret, accessToken, accessTokenSecret);
+	}
+
+	/*public static void main(String[] args) {
 		AmazonKinesis kinesisClient = new AmazonKinesisClient(new CustomCredentialsProviderChain());
 		setupHosebirdClient();
 		hosebirdClient.connect();
 		System.out.println("connected to twitter hose");
+		
 		while (!hosebirdClient.isDone()) {
 			try {
 				String tweetText = msgQueue.take();
@@ -51,7 +110,7 @@ public class TwitterStreamProducer {
 				e.printStackTrace();
 			}
 		}
-    }
+    }*/
 
 	public static void setupHosebirdClient() {
         /** Declare the host you want to connect to, the endpoint, and authentication (basic auth or oauth) */
